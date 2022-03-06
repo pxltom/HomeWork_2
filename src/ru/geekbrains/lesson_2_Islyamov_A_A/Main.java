2abe43737654733454b19d11d061ee7709716df8
pxltom <pixeltom@yandex.ru>
2022-03-06 10:22:27 +0300

    1

--- src/ru/geekbrains/lesson_2_Islyamov_A_A/Main.java
+++ src/ru/geekbrains/lesson_2_Islyamov_A_A/Main.java
@@ -2,6 +2,14 @@ package ru.geekbrains.lesson_2_Islyamov_A_A;
 
 public class Main {
     public static void main(String[] args) {
-            
+        System.out.println(summAandB(-15, 10));
     }
+
+    static boolean summAandB(int a, int b) {
+        return (a + b) >= 10 && (a + b) <= 20;
+    }
+
 }
+
+
+
